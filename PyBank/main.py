5656b94060f3f8ea8c940dd53d1e46e6a20a23f9
David Seger <david.seger@seaboardmarine.com>
2020-08-04 14:18:54 -0500

    rework of logic to main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,19 +12,16 @@ import csv
 
 #define variables
 
-analysis_list = []
-change_list = []
-date_list = []
-row_count =  0
-difference_count = 0
-total_profit_loss = 0
-total_difference = 0
-average_profit_loss = 0.00
-greatest_increase_month = ''
-greatest_increase = 0 
-greatest_decrease_month = ''
-greatest_decrease = 0
-
+total_months =  0                       # Count of Months in csv
+prior_profit_loss = 0                   # Prior Month P/L
+total_profit_loss = 0                   # Total P/L
+total_difference = 0                    # Total Change in P/L by month
+average_change_profit_loss = 0.00       # Avg chg in P/L for periods
+greatest_increase_month = ''            # Greatest increase - Month
+greatest_increase = 0                   # Greatest increase - Dollar
+greatest_decrease_month = ''            # Greatest decrease - Month
+greatest_decrease = 0                   # Greatest decrease - Dollar
+difference = 0                          # Diff between curr/prior month p/l
 
 # Get the path for the budget data file
 
@@ -36,57 +33,66 @@ with open(budget_data) as bankfile:
 
     # this defines teh csv.reader using bankfile and tells it the delimiter
     csvreader = csv.reader(bankfile, delimiter=',')
-
     # Read the header row first as we don't needd that in our list
 
     csv_header = next(csvreader)
  
     # Read each row of data after the header
     for row in csvreader:
-        row_count = row_count + 1
-        total_profit_loss = total_profit_loss + int(row[1])
-        analysis_list.append(int(row[1]))
-        date_list.append(row[0])
-# Now look for the greatest increase/decrease in the list we
-# created while reading the csv file
+        total_months = total_months + 1
+        current_profit_loss = int(row[1])
+        total_profit_loss = total_profit_loss + current_profit_loss
 
-for c in range (1, row_count) :
-    difference = analysis_list[c] - analysis_list[c-1]
-    change_list.append(difference)
-    total_difference = total_difference + difference
-    difference_count = difference_count + 1
-    if difference > greatest_increase:
-        greatest_increase=difference
-        greatest_increase_month = date_list[c]
-    elif difference < greatest_decrease:
-        greatest_decrease = difference
-        greatest_decrease_month = date_list[c]
+        # Now calculate difference in current p/l - prior p/l
+        # Keep running total of cumulative p/l
+         
+        if total_months > 1 :
+            difference =  current_profit_loss - prior_profit_loss
+            total_difference = total_difference + difference
+            cumulative_profit_loss = cumulative_profit_loss + difference
+        else:
+            cumulative_profit_loss=0
+        
+        prior_profit_loss = current_profit_loss
+        
+        # Check difference to see if this is the greatest increase
+        # or decrease so far and if it is store the values
+        if difference > greatest_increase:
+            greatest_increase=difference
+            greatest_increase_month = row[0]
+        elif difference < greatest_decrease:
+            greatest_decrease = difference
+            greatest_decrease_month = row[0]
 
-average_profit_loss = total_difference / difference_count
-average_profit_loss = round(average_profit_loss, 2)
+# Calculate Average Change in P/L
+average_change_profit_loss =  round((cumulative_profit_loss / (total_months-1)),2)
 
 #print results to terminals
 
 print('Financial Analysis')
 print('---------------------------------------------------------------')
-print(f'Total Months . . : {row_count}')
+print(f'Total Months . . : {total_months}')
 print(f'Total  . . . . . : ${total_profit_loss}')
-print(f'Average Change . : ${average_profit_loss}')
+print(f'Average Change . : ${average_change_profit_loss}')
 print(f'Greatest Increase in Profits : {greatest_increase_month} (${greatest_increase})')
 print(f'Greatest Decrease in Profits : {greatest_decrease_month} (${greatest_decrease})')
 
 
 #https://pythonexamples.org/python-write-string-to-text-file/
-#this will write the necessary analysis line to a text file
+#this will write the necessary analysis lines to a text file
 
-analysis_file = open("analysis.txt", "w")
+# Get the path for the analysis text file
 
-n=analysis_file.write('Financial Analysis\n')
-n=analysis_file.write('---------------------------------------------------------------\n')
-n=analysis_file.write(f'Total Months . . : {row_count}\n')
-n=analysis_file.write(f'Total  . . . . . : ${total_profit_loss}\n')
-n=analysis_file.write(f'Average Change . : ${average_profit_loss}\n')
-n=analysis_file.write(f'Greatest Increase in Profits : {greatest_increase_month} (${greatest_increase})\n')
-n=analysis_file.write(f'Greatest Decrease in Profits : {greatest_decrease_month} (${greatest_decrease})\n')
+analysis_file = os.path.join('Analysis', 'analysis.txt')
 
-analysis_file.close()
+analysis_writer = open(analysis_file, "w")
+
+n=analysis_writer.write('Financial Analysis\n')
+n=analysis_writer.write('---------------------------------------------------------------\n')
+n=analysis_writer.write(f'Total Months . . : {total_months}\n')
+n=analysis_writer.write(f'Total  . . . . . : ${total_profit_loss}\n')
+n=analysis_writer.write(f'Average Change . : ${average_change_profit_loss}\n')
+n=analysis_writer.write(f'Greatest Increase in Profits : {greatest_increase_month} (${greatest_increase})\n')
+n=analysis_writer.write(f'Greatest Decrease in Profits : {greatest_decrease_month} (${greatest_decrease})\n')
+
+analysis_writer.close()
